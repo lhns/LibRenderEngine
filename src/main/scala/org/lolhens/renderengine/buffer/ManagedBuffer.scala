e0c573519a310691bbb07d8931d76dc0ac2aa357
LolHens <pierrekisters@gmail.com>
2014-10-14 18:47:50 +0200

    fixed ManagedBuffer

--- src/main/scala/org/lolhens/renderengine/buffer/ManagedBuffer.scala
+++ src/main/scala/org/lolhens/renderengine/buffer/ManagedBuffer.scala
@@ -1,111 +1,159 @@
 package org.lolhens.renderengine.buffer
 
 import java.nio.ByteBuffer
-import java.util.LinkedList
+import java.util
 
 import org.lolhens.renderengine.buffer.ManagedBuffer._
 
 import scala.collection.JavaConversions._
-import scala.collection.mutable.Map
-import scala.util.control.Breaks
+import scala.collection.mutable
 
 /**
  * Created by LolHens on 05.10.2014.
  */
 class ManagedBuffer(buffer: ByteBuffer) {
-  private val mapped = Map[Any, Region]()
-  private val empty = new LinkedList[Region]()
+  private val mapped = mutable.Map[Any, Region]()
+  private val empty = new RegionList()
+  private val dirty = new RegionList()
 
-  empty.add(new Region(0, buffer.capacity()))
+  empty += new Region(0, buffer.capacity())
 
-  private def availableRegion(length: Int): Region = {
-    if (empty.size() == 0 || length > empty.get(empty.length - 1).length) return null
-    var startIndex = 0
-    var endIndex = empty.length - 1
-    var midIndex = 0
-    while (endIndex != startIndex) {
-      midIndex = (startIndex + endIndex) / 2
-      if (length > empty.get(midIndex).length) startIndex = midIndex + 1
-      else endIndex = midIndex
-    }
-    empty.get(startIndex)
-  }
+  def +=(key: Any, bytes: Array[Byte]): Boolean = {
+    if (mapped contains key) this -= key
 
-  private def addEmptyRegion(region: Region): Unit = {
-    val length = region.length
-    if (length < empty.get(0).length) {
-      empty.add(0, region)
-    } else if (length > empty.get(empty.length - 1).length) {
-      empty.add(region)
-    } else {
-      var startIndex = 0
-      var endIndex = empty.length - 1
-      var midIndex = 0
-      while (endIndex != startIndex) {
-        midIndex = (startIndex + endIndex) / 2
-        if (length > empty.get(midIndex).length) startIndex = midIndex + 1
-        else endIndex = midIndex
-      }
-      empty.add(startIndex, region)
-    }
-  }
-
-  private def mergeEmptyRegion(region: Region) = {
-    var pre: Region = null
-    var post: Region = null
-
-    val break = new Breaks
-    break.breakable {
-      for (emptyRegion <- empty) {
-        if (emptyRegion.offset + emptyRegion.length == region.offset) pre = emptyRegion
-        else if (emptyRegion.offset == region.offset + region.length) post = emptyRegion
-        if (pre != null && post != null) break.break()
-      }
-    }
-
-    val offset = if (pre != null) pre.offset else region.offset
-    val length = (if (post != null) post.offset + post.length else region.offset + region.length) - offset
-
-    if (pre != null) empty.remove(pre)
-    if (post != null) empty.remove(post)
-    empty.remove(region)
-
-    addEmptyRegion(new Region(offset, length))
-  }
-
-  def add(key: Any, bytes: Array[Byte]): Boolean = {
-    val region = availableRegion(bytes.length)
+    val region = empty.getSuitableRegion(bytes.length)
     if (region == null) return false
 
-    val newRegion = new Region(region.offset, bytes.length)
+    val bytesRegion = new Region(region.offset, bytes.length)
 
-    buffer.put(bytes, newRegion.offset, newRegion.length)
-    mapped += key -> newRegion
+    buffer.put(bytes, bytesRegion.offset, bytesRegion.length)
 
-    empty.remove(region)
-    if (region.length > newRegion.length) addEmptyRegion(new Region(region.offset + newRegion.length, region.length - newRegion.length))
+    mapped += key -> bytesRegion
+    empty -= bytesRegion
+    dirty += bytesRegion
 
     true
   }
 
-  def remove(key: Any): Boolean = {
+  def -=(key: Any): Boolean = {
     val region = mapped(key)
     if (region == null) return false
 
-    mapped.remove(key)
-    for (i <- region.offset until region.offset + region.length) buffer.put(i, 0)
+    for (i <- region.offset until region.end) buffer.put(i, 0)
 
-    addEmptyRegion(region)
-    mergeEmptyRegion(region)
+    mapped.remove(key)
+    empty += region
+    dirty += region
 
     true
   }
-
-  def defrag() = ???
 }
 
 object ManagedBuffer {
 
-  case class Region(offset: Int, length: Int) {}
+  case class Region(offset: Int, length: Int) {
+    def end = offset + length
+
+    def touches(other: Region): Boolean = other != null && ((other.offset <= end && other.end >= offset) || (other.end >= offset && other.offset <= end))
+
+    def intersects(other: Region): Boolean = other != null && ((other.offset < end && other.end > offset) || (other.end > offset && other.offset < end))
+
+    def contains(other: Region): Boolean = other != null && other.offset >= offset && other.end <= end
+
+    def +=(other: Region): Region = {
+      if (!touches(other)) return null
+      val newOffset = if (other.offset < offset) other.offset else offset
+      val newLength = (if (other.end > end) other.end else end) - newOffset
+      if (newOffset == offset && newLength == length) return this
+      new Region(newOffset, newLength)
+    }
+
+    def -=(other: Region): Array[Region] = {
+      if (!intersects(other)) return null
+      if (other contains this) return Array()
+
+      var pre: Region = null
+      if (other.offset > offset) pre = new Region(offset, other.offset - offset)
+      var post: Region = null
+      if (other.end < end) post = new Region(other.end, end - other.end)
+
+      if (pre != null && post != null)
+        Array(pre, post)
+      else
+        Array(if (pre != null) pre else post)
+    }
+
+    override def toString = s"$offset-$end"
+  }
+
+  class RegionList {
+    private val list = new util.LinkedList[Region]()
+
+    def +=(region: Region): Unit = {
+      if (list.size == 0) list.add(region)
+      else {
+        var newRegion = region
+        val iterator = list.listIterator()
+        var last: Region = null
+        while (iterator.hasNext) {
+          val current = iterator.next
+          if (current.offset > region.offset) {
+            iterator.previous
+            var merged = newRegion += current
+            if (merged != null) {
+              newRegion = merged
+              iterator.remove
+            }
+            merged = newRegion += last
+            if (merged != null) {
+              newRegion = merged
+              iterator.previous
+              iterator.remove
+            }
+            iterator.add(newRegion)
+            return
+          }
+          last = current
+        }
+        val merged = region += list.getLast
+        if (merged != null) {
+          newRegion = merged
+          list.removeLast
+        }
+        list.add(newRegion)
+      }
+    }
+
+    def -=(region: Region): Unit = {
+      val iterator = list.listIterator()
+      while (iterator.hasNext) {
+        val current = iterator.next
+        if (current intersects region) {
+          iterator.previous
+          iterator.remove
+          for (region <- current -= region) iterator.add(region)
+        }
+      }
+    }
+
+    def getSuitableRegion(length: Int): Region = {
+      val iterator = list.iterator
+      var suitable: Region = null
+      while (iterator.hasNext) {
+        val current = iterator.next
+        if (current.length == length)
+          return current
+        else if (current.length > length && (suitable == null || current.length < suitable.length))
+          suitable = current
+      }
+      suitable
+    }
+
+    override def toString = {
+      var string = ""
+      for (region <- list) string = if (string == "") region.toString else s"$string, $region"
+      string
+    }
+  }
 
 }
--- src/main/scala/org/lolhens/renderengine/test/Window.scala
+++ src/main/scala/org/lolhens/renderengine/test/Window.scala
@@ -2,14 +2,22 @@ package org.lolhens.renderengine.test
 
 import java.awt.BorderLayout
 import java.awt.event.{WindowAdapter, WindowEvent}
+import java.nio.{ByteOrder, ByteBuffer}
+import java.util.Random
+import javax.media.opengl._
 import javax.media.opengl.awt.GLCanvas
+import javax.media.opengl.fixedfunc.{GLPointerFunc, GLLightingFunc, GLMatrixFunc}
+import javax.media.opengl.glu.GLU
 import javax.swing.JFrame
 
 /**
  * Created by LolHens on 12.10.2014.
  */
-class Window extends JFrame {
+class Window extends JFrame with GLEventListener {
   val canvas = new GLCanvas()
+  canvas.addGLEventListener(this)
+
+  val glu = new GLU()
 
   addWindowListener(new WindowAdapter() {
     override def windowClosing(event: WindowEvent) = System.exit(0)
@@ -19,4 +27,83 @@ class Window extends JFrame {
   setTitle("TEST WINDOW")
 
   getContentPane().add(canvas, BorderLayout.CENTER)
+
+  override def init(drawable: GLAutoDrawable): Unit = {
+    val gl = drawable.getGL.getGL2
+    drawable.setGL(new DebugGL2(gl))
+
+    gl.glEnableClientState(GLPointerFunc.GL_VERTEX_ARRAY)
+    gl.glEnable(GL.GL_DEPTH_TEST)
+    gl.glDepthFunc(GL.GL_LEQUAL)
+    gl.glShadeModel(GLLightingFunc.GL_SMOOTH)
+    gl.glHint(GL2ES1.GL_PERSPECTIVE_CORRECTION_HINT, GL.GL_NICEST)
+    gl.glClearColor(1f, 0f, 0f, 1f)
+  }
+
+  override def display(drawable: GLAutoDrawable): Unit = {
+
+    val gl = drawable.getGL.getGL2
+    val vboIds = new Array[Int](1)
+    gl.glGenBuffers(1, vboIds, 0)
+    val vboId = vboIds(0)
+    gl.glBindBuffer(GL.GL_ARRAY_BUFFER, vboId)
+
+println(vboId)
+
+    val byteBuffer = ByteBuffer.allocateDirect(3 * 3 * 4).order(ByteOrder.nativeOrder())
+    byteBuffer.putFloat(0.0f)
+    byteBuffer.putFloat(1.0f)
+    byteBuffer.putFloat(0.0f)
+    byteBuffer.putFloat(-1.0f)
+    byteBuffer.putFloat(-1.0f)
+    byteBuffer.putFloat(0.0f)
+    byteBuffer.putFloat(1.0f)
+    byteBuffer.putFloat(-1.0f)
+    byteBuffer.putFloat(0.0f)
+    byteBuffer.rewind()
+    println(byteBuffer)
+    gl.glBufferData(GL.GL_ARRAY_BUFFER, 3 * 3 * 4, byteBuffer, GL.GL_DYNAMIC_DRAW)
+    //gl.glBufferSubData(GL.GL_ARRAY_BUFFER, 0, 3 * 3 * 4, byteBuffer)
+    //while (true) {
+      gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT)
+      gl.glLoadIdentity()
+      gl.glTranslatef(-1.5f, 0.0f, -6.0f)
+      /*gl.glBegin(GL.GL_TRIANGLES)
+      gl.glVertex3f(0.0f, 1.0f, 0.0f)
+      gl.glVertex3f(-1.0f, -1.0f, 0.0f)
+      gl.glVertex3f(1.0f, -1.0f, 0.0f)
+      gl.glEnd()*/
+    //gl.glBindBuffer(GL.GL_ARRAY_BUFFER, vboId)
+    gl.glVertexPointer(3, GL.GL_FLOAT, 4*3, 0)
+      gl.glDrawArrays(GL.GL_TRIANGLES, 0, 3);
+    gl.glBindBuffer(GL.GL_ARRAY_BUFFER, 0)
+      gl.glTranslatef(3.0f, 0.0f, 0.0f)
+      gl.glBegin(GL2GL3.GL_QUADS)
+      gl.glVertex3f(-1.0f, 1.0f, new Random().nextFloat())
+      gl.glVertex3f(1.0f, 1.0f, 0.0f)
+      gl.glVertex3f(1.0f, -1.0f, 0.0f)
+      gl.glVertex3f(-1.0f, -1.0f, 0.0f)
+      gl.glEnd()
+      gl.glFlush()
+      //drawable.swapBuffers()
+    //}
+  }
+
+  override def reshape(drawable: GLAutoDrawable, x: Int, y: Int, width: Int, _height: Int): Unit = {
+    val gl = drawable.getGL.getGL2
+
+    var height = _height
+    if (height <= 0) height = 1
+
+    val h = width.asInstanceOf[Float] / height.asInstanceOf[Float]
+
+    gl.glViewport(0, 0, width, height)
+    gl.glMatrixMode(GLMatrixFunc.GL_PROJECTION)
+    gl.glLoadIdentity()
+    glu.gluPerspective(45.0f, h, 1.0, 20.0)
+    gl.glMatrixMode(GLMatrixFunc.GL_MODELVIEW)
+    gl.glLoadIdentity()
+  }
+
+  override def dispose(drawable: GLAutoDrawable): Unit = {}
 }
