bbad525b650283f862ffc8a6ca97873ad0365440
LolHens <pierrekisters@gmail.com>
2019-01-07 16:40:46 +0100

    more efficient Vector3f

--- src/main/scala/org/lolhens/renderengine/Vector3f.scala
+++ src/main/scala/org/lolhens/renderengine/Vector3f.scala
@@ -3,7 +3,7 @@ package org.lolhens.renderengine
 /**
   * Created by LolHens on 05.10.2014.
   */
-case class Vector3f(x: Float, y: Float, z: Float) {
+case class Vector3f private(x: Float, y: Float, z: Float) {
   def +(vec: Vector3f): Vector3f = Vector3f(x + vec.x, y + vec.y, z + vec.z)
 
   def -(vec: Vector3f): Vector3f = Vector3f(x - vec.x, y - vec.y, z - vec.z)
@@ -17,4 +17,25 @@ case class Vector3f(x: Float, y: Float, z: Float) {
   def min(vec: Vector3f): Vector3f = Vector3f(if (vec.x < x) vec.x else x, if (vec.y < y) vec.y else y, if (vec.z < z) vec.z else z)
 }
 
-object NullVector3f extends Vector3f(0, 0, 0) {}
+object Vector3f {
+  val Zero = new Vector3f(0, 0, 0)
+
+  val One = new Vector3f(1, 1, 1)
+
+  val X = new Vector3f(1, 0, 0)
+
+  val Y = new Vector3f(0, 1, 0)
+
+  val Z = new Vector3f(0, 0, 1)
+
+  @inline
+  def apply(x: Float, y: Float, z: Float): Vector3f =
+    if (x == 0 && y == 0 && z == 0) Zero
+    else if (x == 1 && y == 1 && z == 1) One
+    else new Vector3f(x, y, z)
+
+  @inline
+  def apply(x: Float, y: Float, z: Float, reuse: Vector3f): Vector3f =
+    if (x == reuse.x && y == reuse.y && z == reuse.z) reuse
+    else Vector3f(x, y, z)
+}
--- src/main/scala/org/lolhens/renderengine/model/BoundingBox.scala
+++ src/main/scala/org/lolhens/renderengine/model/BoundingBox.scala
@@ -1,6 +1,6 @@
 package org.lolhens.renderengine.model
 
-import org.lolhens.renderengine.{NullVector3f, Vector3f}
+import org.lolhens.renderengine.Vector3f
 
 /**
   * Created by LolHens on 05.10.2014.
@@ -9,4 +9,4 @@ class BoundingBox(val min: Vector3f, val max: Vector3f) {
   def contain(cube: BoundingBox): BoundingBox = new BoundingBox(min.min(cube.min), max.max(cube.max))
 }
 
-object NullBoundingBox extends BoundingBox(NullVector3f, NullVector3f) {}
+object NullBoundingBox extends BoundingBox(Vector3f.Zero, Vector3f.Zero) {}
